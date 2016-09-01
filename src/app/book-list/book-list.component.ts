e5c26456bfe67a8a25e1a9e2b1d54887d36fec9b
Sascha Brink <sascha.brink@gmail.com>
2016-09-01 16:19:07 +0200

    Create a title @Input

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'book-list',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-list.component.css']
 })
 export class BookListComponent implements OnInit {
+  @Input() title: string;
   isHidden: boolean;
   x: number;
   y: number;
