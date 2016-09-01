ed114ec94e95064cc50847703858e5fb75ae1fd8
Sascha Brink <sascha.brink@gmail.com>
2016-09-01 15:39:36 +0200

    Refactor mousemove event

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookListComponent implements OnInit {
   isHidden: boolean;
+  x: number;
+  y: number;
 
   constructor() {
     this.isHidden = true;
+  }
+
+  onMousemove({offsetX, offsetY}: MouseEvent) {
+    this.x = offsetX;
+    this.y = offsetY;
   }
 
   ngOnInit() {
