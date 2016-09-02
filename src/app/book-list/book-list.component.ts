6ab8d3d8f61d9f1129a579d57678c1de083c393e
Sascha Brink <sascha.brink@gmail.com>
2016-09-02 09:48:53 +0200

    Create a BookData service

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { BookDataService } from '../shared/book-data.service';
 
 @Component({
   selector: 'book-list',
   templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.css']
+  styleUrls: ['./book-list.component.css'],
+  providers: [BookDataService]
 })
 export class BookListComponent implements OnInit {
   @Input() title: string;
@@ -11,9 +13,11 @@ export class BookListComponent implements OnInit {
   isHidden: boolean;
   x: number;
   y: number;
+  books: any[] = []
 
-  constructor() {
+  constructor(private bookData: BookDataService) {
     this.isHidden = true;
+    this.books = bookData.getBooks();
   }
 
   onMousemove({offsetX, offsetY}: MouseEvent) {
