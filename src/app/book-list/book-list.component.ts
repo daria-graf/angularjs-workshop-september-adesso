02d1bd982c16948457be02fee9691e74e15ba408
Sascha Brink <sascha.brink@gmail.com>
2016-09-02 14:28:40 +0200

    Component LifeCycle Basic

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,25 +1,32 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
 import { Book } from '../shared/book';
 import { BookDataService } from '../shared/book-data.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent implements OnInit {
+export class BookListComponent implements OnInit, OnDestroy {
   @Input() title: string;
   @Output() titleClicked = new EventEmitter<string>();
-  isHidden: boolean;
+  isHidden: boolean  = true;
   x: number;
   y: number;
   books: Book[];
+  booksSubscription: Subscription;
 
-  constructor(private bookData: BookDataService) {
-    this.isHidden = true;
-    bookData.getBooks().subscribe((books) => {
+  constructor(private bookData: BookDataService) {}
+
+  ngOnInit() {
+    this.booksSubscription = this.bookData.getBooks().subscribe((books) => {
       this.books = books;
     });
+  }
+
+  ngOnDestroy() {
+    this.booksSubscription.unsubscribe();
   }
 
   onMousemove({offsetX, offsetY}: MouseEvent) {
@@ -29,8 +36,5 @@ export class BookListComponent implements OnInit {
 
   sendTitleClicked() {
     this.titleClicked.emit('Event fired!')
-  }
-
-  ngOnInit() {
   }
 }
